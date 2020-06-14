101e686be13add01916dfc3c2d1cd3a7e8117f84
tunchasan <hasantunc_ogr@outlook.com>
2020-06-14 04:24:31 +0300

    Added new level and created LevelManager script

---
+++ AngryBird-Style-Game/Assets/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private static int _nextLevelIndex = 1;
+
+    private Enemy[] _enemies;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    private void Update()
+    {
+        foreach(Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+                return;
+        }
+
+        _nextLevelIndex++;
+
+        SceneManager.LoadScene("Level" + _nextLevelIndex);
+    }
+}
