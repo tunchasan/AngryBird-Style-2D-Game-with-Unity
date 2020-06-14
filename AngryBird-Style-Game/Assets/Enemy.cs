decbaf9bfee118e542dfc947c353e7aff6cb1e93
tunchasan <hasantunc_ogr@outlook.com>
2020-06-14 03:40:13 +0300

    Created a Enemy script

---
+++ AngryBird-Style-Game/Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird hittedBird = collision.collider.GetComponent<Bird>();
+
+        // Bird hits current enemy
+        if (hittedBird != null)
+        {
+            Destroy(gameObject);
+        }
+
+        Enemy hittedEnemy = collision.collider.GetComponent<Enemy>();
+
+        // Other enemy hits current enemy
+        if (hittedEnemy != null)
+        {
+            return;
+        }
+
+        if(collision.contacts[0].normal.y < -0.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
